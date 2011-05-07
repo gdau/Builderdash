f31b23608b1cffee88cf87e527178bf241467d02
Gavin Dau <g@bitdiff.com>
2011-05-07 11:28:16 +0100

    Added splash

--- src/Builderdash.Client/Program.cs
+++ src/Builderdash.Client/Program.cs
@@ -7,11 +7,19 @@ namespace Builderdash.Client
     class Program
     {
         private readonly static TraceSource Trace = new TraceSource("Builderdash");
-        
+
         static void Main(string[] args)
         {
             try
             {
+                Console.WriteLine(@"
+ _           _ _     _              _           _
+| |         (_) |   | |            | |         | |
+| |__  _   _ _| | __| | ___ _ __ __| | __ _ ___| |__
+| '_ \| | | | | |/ _` |/ _ \ '__/ _` |/ _` / __| '_ \
+| |_) | |_| | | | (_| |  __/ | | (_| | (_| \__ \ | | |
+|_.__/ \__,_|_|_|\__,_|\___|_|  \__,_|\__,_|___/_| |_|");
+
                 new CommandRunner().Run(args);
             }
             catch (Exception e)
